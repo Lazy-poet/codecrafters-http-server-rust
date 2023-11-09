d6e33d31e5886889bc77f985cb05905cfdda7d00
Ro <emmanuelhemarxyll@gmail.com>
2023-11-09 23:16:03 +0100

    http response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,16 +10,28 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+    let port = "4221";
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    println!("listening on port {}", port);
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let content = "Hello World";
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
